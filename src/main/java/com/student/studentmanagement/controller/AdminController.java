4244817026656b20254048714b87a07e2c837e36
Pradipta Dutta <pradiptadutta2005@gmail.com>
2026-02-28 01:54:03 +0530

    Change AdminController

--- src/main/java/com/student/studentmanagement/controller/AdminController.java
+++ src/main/java/com/student/studentmanagement/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.student.studentmanagement.controller;
 import com.student.studentmanagement.dto.UserResponse;
 import com.student.studentmanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.student.studentmanagement.repository.*;
@@ -16,8 +18,11 @@ public class AdminController {
     private final UserRepository userRepository;
     private final SubjectRepository subjectRepository;
     private final UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @PostMapping("/create-user")
     public User createUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
