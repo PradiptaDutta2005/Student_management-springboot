b6ac4eb86257b970f9bcbed0631e7fd19d935cdf
Pradipta Dutta <pradiptadutta2005@gmail.com>
2026-03-03 01:14:31 +0530

    note upload

--- src/main/java/com/student/studentmanagement/controller/StudentController.java
+++ src/main/java/com/student/studentmanagement/controller/StudentController.java
@@ -1,8 +1,17 @@
 package com.student.studentmanagement.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+
 import com.student.studentmanagement.repository.*;
 import com.student.studentmanagement.entity.*;
 
@@ -19,9 +28,20 @@ public class StudentController {
         return attendanceRepository.findByStudent_Id(studentId);
     }
 
-    @GetMapping("/notes/{subjectId}")
-    public List<Notes> getNotes(@PathVariable Long subjectId) {
-        return notesRepository.findBySubjectId(subjectId);
-    }
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) throws Exception {
 
+        Path filePath = Paths.get("uploads").resolve(filename);
+        Resource resource = new UrlResource(filePath.toUri());
+
+        if (!resource.exists()) {
+            throw new RuntimeException("File not found");
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
 }
